26e29ff2ac3622386c9206931bf4b65a2389e803
Vitor Galache <vitorgalache5@gmail.com>
2024-07-15 18:25:17 -0300

    Alterando AppSettings

--- Program.cs
+++ Program.cs
@@ -1,6 +1,21 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
+var connectionString = app.Configuration.GetConnectionString("DefaultConnection");
+var secrets = new Secrets();
+app.Configuration.GetSection("Secrets").Bind(secrets);
 
-app.MapGet("/", () => "Hello World Atualizado!");
+app.MapGet("/", () => new
+{
+    ConnectionString = connectionString,
+    Secrets = secrets
+});
 
 app.Run();
+
+public class Secrets
+{
+    public string JwtTokenSecret { get; set; } = string.Empty;
+    public string ApiKey { get; set; } = string.Empty;
+    public string PrivateKey { get; set; } = string.Empty;
+
+}
